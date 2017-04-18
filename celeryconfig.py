6b3dde0778c616b980392e6489be754591d47ae2
Dmytro Petruk <bavaria95@gmail.com>
2017-04-18 15:49:26 +0200

    celery config file

---
+++ celeryconfig.py
@@ -0,0 +1,5 @@
+BROKER_URL = 'pyamqp://guest:guest@188.184.92.92:5672//'
+# BROKER_URL = 'pyamqp://guest:guest@188.184.94.146:5672//'
+
+CELERY_RESULT_BACKEND = 'redis://:@188.184.92.92:7000/0'
+# CELERY_RESULT_BACKEND = 'redis://:@188.184.94.146:7000/0'
